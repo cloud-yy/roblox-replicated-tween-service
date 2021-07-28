e0e534949256fc0c4d6467b791e1418d5d1f4ac2
cloud-yy <36166667+cloud-yy@users.noreply.github.com>
2021-07-28 11:17:59 +0100

    Rewritten the entire service almost.
    Supports more tween methods, works in a more similar way the tween service. There may be some bugs though.

--- ReplicatedTweening.lua
+++ ReplicatedTweening.lua
@@ -8,9 +8,9 @@
 ]]--
 -----------------------------------------
 
--- Name: Replicated Tweening
+-- Name: ReplicatedTweenService
 -- By: cloud_yy
--- Date: Sunday, July 25 2021
+-- Date: Tuesday, July 27 2021
 
 --[[
 
@@ -21,17 +21,20 @@ while also looking nice and smooth on the client side. This is not a perfect rec
 Documentation: 
 SERVER
 local ReplicatedTweening = require(ReplicatedTweening)
-Starts ReplicatedTweening on the server.
+    Starts ReplicatedTweening on the server.
 
 local Tween = ReplicatedTweening.new(Object, Info, Goal)
     Tween:Play()
-    Plays or resumes the tween.
-
-    Tween:Pause()
-    Pauses the tween to be played later.
+        Plays or resumes the tween.
 
     Tween:Stop()
-    Fully cancels and stops the tween.
+        Stops the tween.
+
+    Tween:Pause()
+        Pauses the tween to be played later.
+
+    Tween:Cancel()
+        Cancels the tween.
 
 CLIENT
 require(ReplicatedTweening)
@@ -39,11 +42,12 @@ Starts Replicated Tweening on the client.
 
 ]]--
 
-local RunService = game:GetService("RunService")
 local TweenService = game:GetService("TweenService")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
+local HttpService = game:GetService("HttpService")
 
-local Intents = {Play = 1, Pause = 2, Stop = 3}
+local Intents = {New = 1, Sync = 2, Play = 3, Stop = 4, Pause = 5, Cancel = 6}
+local Tweens = {}
 local Event
 
 local function InfoToTable(Info)
@@ -61,87 +65,97 @@ local function TableToInfo(Table)
 	return TweenInfo.new(unpack(Table))
 end
 
-local ReplicatedTweening = {}
+local function CreateLocalTween(Id, Object, Info, Goal)
+    local Tween = TweenService:Create(Object, TableToInfo(Info), Goal)
+    Tweens[Id] = Tween
+end
+
+local ReplicatedTweenService = {}
 
 if RunService:IsServer() then
     Event = Instance.new("RemoteEvent",script)
 
-    ReplicatedTweening.__index = ReplicatedTweening
+    Event.OnServerEvent:Connect(function(Player,Intent)
+        if Intent == Intents.Sync then
+            Event:FireClient(Player,Tweens)
+        end
+    end)
+
+    ReplicatedTweenService.__index = ReplicatedTweenService
     
-    function ReplicatedTweening:Create(Object, Info, Goal)
-        local self = setmetatable({}, ReplicatedTweening)
+    function ReplicatedTweenService:Create(Object, Info, Goal)
+        local self = setmetatable({}, ReplicatedTweenService)
 
-        self.Object = Object
-        self.Info = Info
-        self.Goal = Goal
+        self._id = HttpService:GenerateGUID()
+        self._tween = TweenService:Create(Object, Info, Goal)
 
-        self.ServerTween = TweenService:Create(Object, Info, Goal)
-        self.ClientTween = {Object, InfoToTable(Info), Goal}
-
+        Tweens[self._id] = {Object, InfoToTable(Info), Goal}
+        Event:FireAllClients(Intents.New, self._id, Object, InfoToTable(Info), Goal)
         return self
     end
     
-    function ReplicatedTweening:Play()
-        self.ServerTween:Play()
-        Event:FireAllClients(Intents.Play,self.ClientTween)
-    end
-
-    function ReplicatedTweening:Pause()
-        self.ServerTween:Pause()
-        Event:FireAllClients(Intents.Pause,self.ClientTween)
-    end
-
-    function ReplicatedTweening:Stop()
-        self.ServerTween:Stop()
-        Event:FireAllClients(Intents.Stop,self.ClientTween)
+    function ReplicatedTweenService:Play()
+        self._tween:Play()
+        Event:FireAllClients(Intents.Play, self._id)
     end
     
-    return ReplicatedTweening
+    function ReplicatedTweenService:Stop()
+        self._tween:Stop()
+        Event:FireAllClients(Intents.Stop, self._id)
+    end
+
+    function ReplicatedTweenService:Pause()
+        self._tween:Pause()
+        Event:FireAllClients(Intents.Pause, self._id)
+    end
+
+    function ReplicatedTweenService:Cancel()
+        self._tween:Cancel()
+        Event:FireAllClients(Intents.Cancel, self._id)
+    end
 else
-    local Tweens = {}
-    local PausedTweens = {}
-
     Event = script:WaitForChild("RemoteEvent")
-    Event.OnClientEvent:Connect(function(Intent, TweenData)
 
-        local Object, Info, Goal = unpack(TweenData)
+    Event.OnClientEvent:Connect(function(Intent,...)
+        local Args = {...}
+        if Intent == Intents.New then
+            CreateLocalTween(...)
 
-        if Intent == Intents.Play then
-            if Tweens[Object] then
-                local Tween = Tweens[Object]
-                
-                if PausedTweens[Object] then
-                    PausedTweens[Object]:Play()
-                    PausedTweens[Object] = nil
-                    return
-                else
-                    Tween[Object]:Cancel()
-                    Tween[Object] = nil
-                end
+        elseif Intent == Intents.Sync then
+            for Id,Tween in pairs(Args[1]) do
+                CreateLocalTween(Id,Tween[1],Tween[2],Tween[3])
             end
-            local Tween = TweenService:Create(Object,TableToInfo(Info),Goal)
-            Tweens[Object] = Tween
-            Tween:Play()
-        elseif Intent == Intents.Pause then
-            if Tweens[Object] then
-                PausedTweens[Object] = Tweens[Object]
-                Tweens[Object]:Pause()
+
+        elseif Intent == Intents.Play then
+            local Tween = Tweens[Args[1]]
+            if Tween then
+                Tween:Play()
             end
 
         elseif Intent == Intents.Stop then
-            if Tweens[Object] then
-                Tweens[Object]:Cancel()
-                Tweens[Object] = nil
+            local Tween = Tweens[Args[1]]
+            if Tween then
+                Tween:Stop()
             end
 
-            if PausedTweens[Object] then
-                PausedTweens[Object] = nil
+        elseif Intent == Intents.Pause then
+            local Tween = Tweens[Args[1]]
+            if Tween then
+                Tween:Pause()
+            end
+
+        elseif Intent == Intents.Cancel then
+            local Tween = Tweens[Args[1]]
+            if Tween then
+                Tween:Cancel()
             end
         end
     end)
 
-    return ReplicatedTweening
+    Event:FireServer(Intents.Sync)
 end
 
--- Replicated Tweening
+return ReplicatedTweenService
+
+-- ReplicatedTweenService
 -- © 2021 cloud_yy
